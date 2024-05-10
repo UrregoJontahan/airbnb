a718801a1e56d40aec266c1939ec32caa4f7a6c7
jonathan urrego <jdavid598@hotmail.com>
2024-05-10 09:11:06 -0500

    error

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,26 +1,57 @@
-import axios from "axios"
+"use client"
 
-export async function Login(userData:any){
+import Link from "next/link"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-  try{
-    const response = await axios.post("http://localhost:2000/users", userData)
-    return response.data
-  } catch{
-    console.log("Ay,noooooooooooooooooooooooooooooooooooooooooo")
+export function Login(){
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  
+  const handleSubmitLogin = async (e:any) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:2000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        console.log("Inicio de sesión exitoso");
+        router.push("/"); 
+      } else {
+        console.error('Credenciales incorrectas');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
   }
-
-  // const handleLogin = (e) => {
-  //   e.preventDefault()
-  // }
-
    return(
-    <div className="block mx-96 mt-20 max-w-screen-md">
-    <h1 className="text-4xl font-bold mb-12 bg-gradient-to-r from-indigo-600 to-pink-500 text-transparent bg-clip-text flex justify-center">Login</h1>
-    <form className="grid gap-x-4 gap-y-8" method="POST">
-      <input type="text" name="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" className="h-4 rounded-lg p-8 text-black focus:border-violet-500" />
-      <input type="password" name="password" placeholder="password" className="h-4 rounded-lg p-8 text-black" />
-      <input type="submit" name="submit" value="Login" className="bg-violet-neon hover:bg-blue-500 cursor-pointer h-12 rounded-lg"/>
-    </form>
+    <div className="flex items-center justify-center h-screen">
+    <div className="w-2/5 h-4/5 rounded-md border border-gray-400 shadow-xl shadow-neutral-700">
+      <div className="text-base font-medium flex">
+        <Link href={"/"} className="flex pt-4 pl-8">x</Link>
+        <span className="flex text-base font-medium p-4 relative left-1/3">Iniciar Sesión</span>
+      </div>
+      <div className="flex w-full border-t border-gray-300 flex-col">
+        <h2 className="font-medium flex p-4 pl-8 ">Te damos la bienvenida a Airbnb</h2>
+        <form className="flex flex-col pl-8 pr-8" method="POST" onSubmit={handleSubmitLogin}>
+          <input className="w-full h-10 border border-gray-400 rounded-t-lg pl-4" type="text" value={username} placeholder="Nombre de Usuario" onChange={(e) => setUsername(e.target.value)} required/>
+          <input className="w-full h-10 border-l border-r border-gray-400 pl-4" placeholder="Numero de telefono" type="text"  />
+          <input className="w-full h-10 border border-gray-400 pl-4 rounded-b-lg" type="password" value={password} placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)} />
+          <p className="text-xs pt-2"> Te vamos a confirmar el numero por telefono o mensaje de texto.Sujeto a las tarifas estandar para mensajes y datos</p>
+          <input type="submit" className="w-full h-10 border bg-pink-600 rounded-lg mt-4 text-white font-medium cursor-pointer" value="Continúa"/>
+          <span className="flex justify-center pt-4">o</span>
+          <div className="flex flex-col pt-8 gap-4">
+            <button className="w-full h-10 border border-gray-400 rounded-lg text-black cursor-pointer hover:bg-gray-200">Continúa con Facebook</button>
+            <button className="w-full h-10 border border-gray-400 rounded-lg pl-4 cursor-pointer hover:bg-gray-200">Continúa con Google</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
    )
 }
--- src/components/Singup/SingupUser.tsx
+++ src/components/Singup/SingupUser.tsx
@@ -1,14 +1,16 @@
 "use client"
 import Link from "next/link"
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const NewUserAccount =() => {
   const [name, setName] = useState('');
   const [celphone, setCelphone] = useState('');
   const [password, setPassword] = useState('');
-  
-
-    const handleSubmitSingup = async (e:any) => {
+  const router = useRouter()
+ 
+ 
+  const handleSubmitSingup = async (e:any) => {
       e.preventDefault();
       try {
         const response = await fetch('http://localhost:2000/users', {
@@ -19,7 +21,8 @@ export const NewUserAccount =() => {
           body: JSON.stringify({ name, celphone, password }),
         });
         if (response.ok) {
-         console.log("bien")
+        console.log("bien")
+          router.push("/login")
       } else {
       console.error('Error al registrar usuario');
       }
@@ -27,7 +30,6 @@ export const NewUserAccount =() => {
     console.error('Error de red:', error);
     }
 }
-
     return(
       <div className="flex items-center justify-center h-screen">
         <div className="w-2/5 h-4/5 rounded-md border border-gray-400 shadow-xl shadow-neutral-700">
@@ -37,8 +39,7 @@ export const NewUserAccount =() => {
           </div>
           <div className="flex w-full border-t border-gray-300 flex-col">
             <h2 className="font-medium flex p-4 pl-8 ">Te damos la bienvenida a Airbnb</h2>
-
-            <form className="flex flex-col pl-8 pr-8" method="POST" onSubmit={handleSubmitSingup}>
+            <form className="flex flex-col pl-8 pr-8" method="POST" onSubmit={handleSubmitSingup} action={""}>
               <input className="w-full h-10 border border-gray-400 rounded-t-lg pl-4" type="text" value={name} placeholder="Nombre de Usuario" onChange={(e)=>setName(e.target.value)} required/>
               <input className="w-full h-10 border-l border-r border-gray-400 pl-4" placeholder="Numero de telefono" type="text" value={celphone} onChange={(e)=>setCelphone(e.target.value)} required/>
               <input className="w-full h-10 border border-gray-400 pl-4 rounded-b-lg" type="password" value={password} placeholder="Contraseña" onChange={(e)=>setPassword(e.target.value)} required/>
@@ -50,7 +51,6 @@ export const NewUserAccount =() => {
                 <button className="w-full h-10 border border-gray-400 rounded-lg pl-4 cursor-pointer hover:bg-gray-200">Continúa con Google</button>
               </div>
             </form>
-
           </div>
         </div>
       </div>
